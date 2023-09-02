6b6991efae2edaf32c52916aa821a5cf1916cd10
tolgabayrakdev <bayraktolga28@gmail.com>
2023-09-02 10:34:54 +0300

    rewgister method added

--- server/app/controller/auth_controller.py
+++ server/app/controller/auth_controller.py
@@ -1,15 +1,19 @@
 from fastapi import APIRouter
 from fastapi import Request, Response, HTTPException
 from service.auth_service import AuthService
-from schema.user_schema import UserLogin
+from schema.user_schema import UserLogin, UserRegister
 
 auth_router = APIRouter()
 
 
-@auth_router.post("/")
-async def test(user: UserLogin, response: Response):
+@auth_router.post("/login")
+async def login(user: UserLogin, response: Response):
     data = await AuthService.login(user.email, user.password)
     if data is None:
         return HTTPException(status_code=400, detail="Username or password wrong!")
-    response.set_cookie(key="access_token", value=data[""])
+    response.set_cookie(key="access_token", value=data["access_token"])
     return {"data": {"email": data.email}}
+
+@auth_router.route("/register")
+async def register(user:  UserRegister, response: Response):
+    return 
--- server/app/service/auth_service.py
+++ server/app/service/auth_service.py
@@ -3,13 +3,17 @@ from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 from fastapi import HTTPException
 from util.helper import Helper
-class AuthService:
+from typing import Any
 
+
+class AuthService:
     @staticmethod
     async def login(email: str, password: str):
         try:
             hash_password = Helper.generate_hash_password(password=password)
-            query = text("SELECT * FROM users WHERE email = :email and password = :password")
+            query = text(
+                "SELECT * FROM users WHERE email = :email and password = :password"
+            )
             result = connection.execute(query, email=email, password=hash_password)
             print(result)
             for row in result:
@@ -18,4 +22,20 @@ class AuthService:
         except SQLAlchemyError as e:
             raise HTTPException(status_code=500, detail="Database Error!")
 
-            
+    @staticmethod
+    async def register(data: Any):
+        try:
+            hash_password = Helper.generate_hash_password(password=data.password)
+            query = text(
+                """
+                INSERT INTO users(username, email, password)
+                VALUES(%s, %s, %s)
+                """
+            )
+            result = connection.execute(query, data.username, data.email, hash_password)
+            for row in result:
+                print(row)
+                return row
+
+        except SQLAlchemyError as e:
+            raise HTTPException(status_code=500, detail="Database Error!")
